0ea433fd23243d2b9501aeea5b5a6c47144dc5cf
Amandine <amandine.labaeye@orange.fr>
2018-11-13 16:15:42 +0100

    commit

--- Pendu.js
+++ Pendu.js
@@ -2,22 +2,22 @@
 // Tableau avec les mots à deviner
 
 let motsadeviner = [
-  "ORDINATEUR",
-    "TELEPHONE",
-    "TOUR",
-    "SOURIS",
-    "ASCENCEURS",
-    "ASSASSINS",
-    "FUSIL",
-    "COCA",
-    "ICE",
-    "PAIN",
-    "CROISSANT",
-    "STYLO",
-    "BOUTEILLE",
-    "COUPLE",
-    "CLAVIER",
-    "DOUDOU"
+  "ordinateur",
+    "telephone",
+    "tour",
+    "souris",
+    "ascenceurs",
+    "assassins",
+    "fusil",
+    "coca",
+    "ice",
+    "pain",
+    "croissant",
+    "stylo",
+    "bouteille",
+    "couple",
+    "clavier",
+    "doudou"
 ];
 
 
@@ -25,74 +25,156 @@ let motsadeviner = [
 
 let AleatoireMots = motsadeviner[Math.floor(Math.random() * motsadeviner.length)];
 
-document.getElementById("Aleatoire").innerHTML = AleatoireMots;
+console.log(AleatoireMots);
+// Pour le 'Cest gagnée'
 
-for (let i = 0; i <= AleatoireMots.length; i++) {
+let Rep = 0;
 
-    document.getElementById('lettre' + i).innerHTML = "_";
+
+// Nombre de vie au départ
+
+let vie = 0;
+
+
+function Inpuut () {
+
+    // Vider l'input après proposition
+
+    document.getElementById("Input").value = "";
 
 }
 
+function Focuus () {
+
+    document.getElementById("Input").focus();
+
+}
 
 // Evenement click sur le bouton Valider
 
 document.getElementById("Valider").addEventListener("click", function () {
 
 
+    // On recupere la valeur de l'input
+
+    let Recuperer = document.getElementById("Input").value;
+
+
+    // Affiche la lettre saisie dans l'input
+
+    document.getElementById('Valeur').innerHTML = Recuperer;
+
     // Boucle pour les résultats et indications
 
     for (let i = 0; i < AleatoireMots.length; i++) {
 
-
-        // On recupere la valeur de l'input
-
-        let Recuperer = document.getElementById("Input").value;
-
-
-        // Affiche la lettre saisie dans l'input
-
-        document.getElementById('Valeur').innerHTML = Recuperer;
-
-
-        // Indiquer au joueur si La lettre est bonne ou non
-
-        // Si la lettre entrée dans l'input est strictement égale à -1 (-1 vaut la valeur de la lettre qui n'existe pas dans la chaine)
-        if (AleatoireMots.indexOf(Recuperer) === -1) {
-
-
-            // alors la lettre proposé n'est pas bonne
-            document.getElementById("Indication").innerHTML = "La lettre proposé n'est pas dans le mot"
-
-        } else {
-
-
-            // Sinon la lettre proposé fait parti du mot
-            document.getElementById("Indication").innerHTML = "La lettre proposé est dans le mot";
-
-        }
-
-        // Affiche la lettre si elle est bonne ou affiche rien
-
-        // Si la valeur est strictement égale au mot aleatoire ++
         if (Recuperer === AleatoireMots[i]) {
 
-            // alors ca mets la lettre qui est bonne
+            // alors ca mets la lettre qui est
+
             document.getElementById("lettre" + i).innerHTML = " " + AleatoireMots[i];
+
+            // Incrémentation pour qui comptabilise que les bonnes réponses
+
+            Rep++;
 
         } else {
 
 
             // Sinon ca afficher une barre vide
+
             document.getElementById("lettre" + i).innerHTML += " ";
 
         }
 
+
+        if (Rep === AleatoireMots.length) {
+
+            document.getElementById("Resultat").innerHTML = "Bravo tu as gagné tu as utilisé " + vie  + " vies sur 10, tu peux générer un nouveau mot en cliquant sur le bouton 'Reset'";
+            document.getElementById("Input").style.display = "none";
+            document.getElementById("Valider").style.display = "none";
+        }
+
     }
 
-    // Vider l'input après proposition
+    if (AleatoireMots.indexOf(Recuperer) === -1) {
 
-    document.getElementById("Input").value = "";
+        document.getElementById("Indication").innerHTML = "La lettre proposé n'est pas dans le mot";
+
+
+        // Incrémentation quand on click sur le bouton une vie s'enlève
+
+        vie++;
+        console.log(vie);
+
+
+    }
+
+
+    // Affiche le nombre de tentatives qui reste à l'utilisateur
+
+    document.getElementById("Chance").innerHTML = "Vous avez utilisé " + vie + " vies sur 10";
+
+    if (vie >= 10) {
+
+        document.getElementById("Resultat").innerHTML = "Vous avez perdu le mot était " + AleatoireMots + " Pour avoir un nouveau mot clique sur le bouton 'Reset'";
+        document.getElementById("Input").style.display = "none";
+        document.getElementById("Valider").style.display = "none";
+
+    }
+
+    Inpuut ();
+
+    Focuus ();
 
 });
 
 
+// Boucle pour avoir le nombre de tirer en fonction du nombre de lettre dans le mot
+
+for (let i = 0; i < AleatoireMots.length; i++) {
+
+    document.getElementById('lettre' + i).innerHTML = "_";
+
+}
+
+
+// Bouton Reset
+
+document.getElementById('Reset').addEventListener("dblclick", function () {
+
+    location.reload();
+
+});
+
+
+
+/*
+document.getElementById("Facile").addEventListener("click", function () {
+
+    AleatoireMots = motsadeviner[Math.floor(Math.random() * motsadeviner.length)];
+
+    console.log(AleatoireMots);
+
+
+
+});
+
+document.getElementById("Normal").addEventListener("click", function () {
+
+    AleatoireMots = motsadeviner[Math.floor(Math.random() * motsadeviner.length)];
+
+    console.log(AleatoireMots);
+
+});
+
+document.getElementById("Difficile").addEventListener("click", function () {
+
+    AleatoireMots = motsadeviner[Math.floor(Math.random() * motsadeviner.length)];
+
+    console.log(AleatoireMots);
+
+
+});
+
+*/
